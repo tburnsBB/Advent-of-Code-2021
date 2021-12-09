845ed1301d17b5b17d109687d6ccdd84c1fa4cdb
tburnsBB <41926756+tburnsBB@users.noreply.github.com>
2021-12-08 20:51:10 -0600

    updating

--- 2020/Day 4/Day4.js
+++ 2020/Day 4/Day4.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
 var file = fs.readFileSync('credentials.txt', 'utf8');
-var credentials = file.split('\n\n');
-console.log(credentials[0]);
+var credentials = file.split('\n');
+var currentPassport = [];
+var RelevantField;
+(function (RelevantField) {
+    RelevantField["byr"] = "byr";
+    RelevantField["iyr"] = "iyr";
+    RelevantField["eyr"] = "eyr";
+    RelevantField["hgt"] = "hgt";
+    RelevantField["hcl"] = "hcl";
+    RelevantField["ecl"] = "ecl";
+    RelevantField["pid"] = "pid";
+})(RelevantField || (RelevantField = {}));
+console.log('Starting here =============================================================');
+for (var _i = 0, credentials_1 = credentials; _i < credentials_1.length; _i++) {
+    var cred = credentials_1[_i];
+    if (cred.length === 1) {
+        console.log(currentPassport);
+        console.log('-------------------');
+        currentPassport = [];
+    }
+    if (cred.length > 1) {
+        currentPassport += cred;
+        console.log(currentPassport);
+    }
+}
--- 2020/Day 4/Day4.ts
+++ 2020/Day 4/Day4.ts
@@ -1,5 +1,30 @@
 let fs = require('fs');
 let file = fs.readFileSync('credentials.txt', 'utf8');
-let credentials = file.split('\n\n');
+let credentials = file.split('\n');
+let currentPassport: string[] = [];
 
-console.log(credentials[0]);
+
+enum RelevantField {
+    byr = 'byr',
+    iyr = 'iyr',
+    eyr = 'eyr',
+    hgt = 'hgt',
+    hcl = 'hcl',
+    ecl = 'ecl',
+    pid = 'pid',
+}
+console.log('Starting here =============================================================');
+for (let cred of credentials) {
+    if (cred.length === 1) {
+        console.log(currentPassport);
+        console.log('-------------------');
+        currentPassport = [];
+    }
+
+    if (cred.length > 1) {
+        currentPassport += cred
+        console.log(currentPassport);
+    }
+
+
+}
